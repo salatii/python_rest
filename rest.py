23f43013c2fb705734b8db80a078c255ec5137cf
Katii <49525676+salatii@users.noreply.github.com>
2023-07-14 13:02:43 +0200

    Add files via upload

--- rest.py
+++ rest.py
@@ -1,79 +1,160 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import feedparser
+import langid
 from keybert import KeyBERT
 from transformers import pipeline
+from transformers import MarianMTModel, MarianTokenizer
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import LineTokenizer
+import math
 import requests
+import torch
+import nltk
+import os
+
+#nltk.download('punkt')
+os.environ['CURL_CA_BUNDLE'] = ''
 
 requests.Session().verify = False
 
-app = Flask(__name__)
+if torch.cuda.is_available():
+    dev = "cuda"
+else:
+    dev = "cpu"
+device = torch.device(dev)
 
-@app.route("/")
-def hello_world():
-    #TODO welche uuids muss man tätigen
-    return "Hello, World!"
+# TODO execute before first request and load models
+def init_models():
+    print('hi')
 
-@app.route('/<uuid>', methods=['GET', 'POST'])
-def add_message(uuid):
-    content_type = request.headers.get('Content-Type')
-    content_check = check_content_type(content_type)
-    json = None
-    if content_check:
-        json = request.json
-    print(uuid)
-    print(json)
-    print(json is not None)
 
-    if uuid == 'rss':
-        return get_rss()
-    elif uuid == 'sentiment':
-        if json is not None:
-            return get_sentiment(json['content'])
+def create_app():
+    app = Flask(__name__)
+    init_models()
+
+    @app.route("/")
+    def hello_world():
+        # TODO welche uuids muss man tätigen
+        return "Help Information Incomming"
+
+    @app.route('/<uuid>', methods=['GET', 'POST'])
+    def add_message(uuid):
+        content_type = request.headers.get('Content-Type')
+        content_check = check_content_type(content_type)
+        data = None
+        if content_check:
+            data = request.get_json()
+
+        if uuid == 'rss':
+            return get_rss()
+        elif uuid == 'detectLng':
+            if data is not None:
+                return get_language(data)
+            else:
+                return 'error'
+        elif uuid == 'sentiment':
+            if data is not None:
+                return get_sentiment(data)
+            else:
+                return 'error'
+        elif uuid == 'summarize':
+            if data is not None:
+                return get_summary(data)
+            else:
+                return 'error'
+        elif uuid == 'keywords':
+            if data is not None:
+                return get_keywords(data)
+            else:
+                return 'error'
+        elif uuid == 'translate':
+            if data is not None:
+                return get_translation(data)
+            else:
+                return 'error'
         else:
             return 'error'
-    elif uuid == 'summarize':
-        if json is not None:
-            return get_summary(json['content'])
-        else:
-            return 'error'
-    elif uuid == 'keywords':
-        if json is not None:
-            return get_keywords(json['content'])
-        else:
-            return 'error'
-    elif uuid == 'translate':
-        if json is not None:
-            return 'translate'
-        else:
-            return 'error'
-    else:
-        return 'error'
+
+    return app
+
 
 def check_content_type(content_type):
-    if (content_type == 'application/json'):
+    if content_type == 'application/json':
         return True
     else:
         return False
 
+
 def get_rss():
     rss_url = 'https://www.heise.de/rss/heise.rdf'
     feed = feedparser.parse(rss_url).entries
-    #return json.dumps(feed)
+    # return json.dumps(feed)
     return feed
 
-def get_keywords(content):
+
+def get_keywords(body):
+    input = body.get('input')
     kw_model = KeyBERT()
-    keywords = kw_model.extract_keywords(content)
-    print(keywords)
-    return keywords
+    body['output'] = keywords = kw_model.extract_keywords(input)
+    return jsonify(body)
 
-def get_summary(content):
+
+def get_summary(body):
+    input = body.get('input')
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    return summarizer(content, max_length=130, min_length=30, do_sample=False)
+    body['output'] = summarizer(input, max_length=130, min_length=30, do_sample=False)
+    return jsonify(body)
 
-def get_sentiment(content):
-    print("Hallo")
+
+def get_sentiment(body):
+    input = body.get('input')
     classifier = pipeline("sentiment-analysis")
-    print("Hallo1")
-    return classifier(content)
+    body['output'] = classifier(input)
+    return jsonify(body)
 
+
+def get_language(body):
+    input = body.get('input')
+    detected_language, confidence = langid.classify(input)
+    body['output'] = detected_language, confidence
+    return jsonify(body)
+
+
+def get_translation(body):
+    translation_en_de = pipeline("translation_en_to_de")
+    input = body.get('input')
+    mname = 'Helsinki-NLP/opus-mt-de-en'
+    tokenizer = MarianTokenizer.from_pretrained(mname)
+    model = MarianMTModel.from_pretrained(mname)
+    model.to(device)
+
+    if input:
+        if body['action'] == 'translate_en_de':
+            output = translation_en_de(input, max_length=512)
+            body['output'] = output[0]['translation_text']
+        if body['action'] == 'translate_de_en':
+            lt = LineTokenizer()
+            batch_size = 8
+            paragraphs = lt.tokenize(input)
+            translated_paragraphs = []
+
+            for paragraph in paragraphs:
+                sentences = sent_tokenize(paragraph)
+                batches = math.ceil(len(sentences) / batch_size)
+                translated = []
+                for i in range(batches):
+                    sent_batch = sentences[i * batch_size:(i + 1) * batch_size]
+                    model_inputs = tokenizer(sent_batch, return_tensors="pt", padding=True, truncation=True).to(device)
+                    with torch.no_grad():
+                        translated_batch = model.generate(**model_inputs)
+                    translated += translated_batch
+                translated = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+                translated_paragraphs += [" ".join(translated)]
+            translated_text = "\n".join(translated_paragraphs)
+            body['output'] = translated_text
+    return jsonify(body)
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
--- translate.py
+++ translate.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from transformers import MarianMTModel, MarianTokenizer
 import os
 os.environ['CURL_CA_BUNDLE'] = ''
@@ -34,4 +35,42 @@ translated_back = ''
 for t in translated:
     translated_back += (tokenizer_EN_DE.decode(t, skip_special_tokens=True))
 
+=======
+from transformers import MarianMTModel, MarianTokenizer
+import os
+os.environ['CURL_CA_BUNDLE'] = ''
+
+text_DE = "Hallo kannst du mir diesen Text übersetzen. Ich bin leider nicht so gut in Englisch."
+
+#Helsinki-NLP/opus-mt-de-en
+model_path = ".\models\DE_EN"
+tokenizer_DE_EN = MarianTokenizer.from_pretrained(model_path)
+model_DE_EN = MarianMTModel.from_pretrained(model_path)
+
+translated = model_DE_EN.generate(**tokenizer_DE_EN(text_DE, return_tensors="pt", padding=True))
+
+#model.save_pretrained(".\models\DE_EN")
+#tokenizer_DE_EN.save_pretrained(".\models\DE_EN")
+
+translated_str = ''
+for t in translated:
+    translated_str += (tokenizer_DE_EN.decode(t, skip_special_tokens=True))
+
+print(translated_str)
+#-------------------------------------------------------------------------------------------------------------
+#Helsinki-NLP/opus-mt-en-de
+model_path = ".\models\EN_DE"
+tokenizer_EN_DE = MarianTokenizer.from_pretrained(model_path)
+model_EN_DE = MarianMTModel.from_pretrained(model_path)
+
+translated = model_EN_DE.generate(**tokenizer_EN_DE(translated_str, return_tensors="pt", padding=True))
+
+#model_EN_DE.save_pretrained(".\models\EN_DE")
+#tokenizer_EN_DE.save_pretrained(".\models\EN_DE")
+
+translated_back = ''
+for t in translated:
+    translated_back += (tokenizer_EN_DE.decode(t, skip_special_tokens=True))
+
+>>>>>>> 20fda42b756a8e6715294efb3c725f4edc168d34
 print(translated_back)
